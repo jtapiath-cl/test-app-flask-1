cf9e32a5e8027e88e279d51048afff7b6e6fbeac
Javier <javier.tapiath@gmail.com>
2020-05-12 09:47:28 -0400

    added a download file button and API endpoint

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-def image_generator(texto: str):
+def image_generator(texto: str, firma:str = "@raspado_de_olla"):
     from PIL import Image, ImageDraw, ImageFont
     import textwrap
     
     font_loc = "static/resources/Code New Roman.otf"
     footer_loc = "static/resources/Code New Roman b.otf"
     fontsize = 60
-    footer = "@raspado_de_olla"
+    footer = firma
     
     img = Image.new("RGB", (1080, 1080), color = "black")
     canvas = ImageDraw.Draw(img)
@@ -19,8 +19,8 @@ def image_generator(texto: str):
     
     return img
 
-from flask import Flask, render_template, request, send_file, send_from_directory
-from flask import jsonify
+from flask import Flask, render_template, request, send_file
+from flask import jsonify, abort
 import os
 
 IMG_REPO = os.path.join("static", "imgs")
@@ -36,11 +36,23 @@ def home():
 def generating_image():
     from datetime import datetime
     text_form = request.form.get("textBox1")
+    signature = request.form.get("textBox2")
     img_name = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S") + "_img.png"
     img_path = os.path.join(app.config["IMG_FOLDER"], img_name)
-    imagen = image_generator(text_form)
+    imagen = image_generator(texto = text_form, firma = signature)
     imagen.save(img_path)    
     return render_template("img.html", user_image = img_path)
+
+@app.route("/get-image/<path:path>")
+def serving_image(path):
+    from datetime import datetime
+    file_to_download = path
+    img_name = datetime.strftime(datetime.now(), "%Y%m%d-%H%M%S") + "-meme.png"
+    if os.path.exists(file_to_download):
+        return send_file(file_to_download, cache_timeout = 0, as_attachment = True,
+                          attachment_filename = img_name)
+    else:
+        abort(404, "file does not exist")
 
 if __name__ == "__main__":
     app.run(threaded = True, debug = True, port = 1337)
